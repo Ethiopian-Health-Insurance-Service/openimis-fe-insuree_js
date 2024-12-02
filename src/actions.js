ca82934d78b63774985a2a906c4c24c6b473f571
michel-lewis <kouamkouammichellewis@gmail.com>
2024-12-02 12:42:07 +0100

    fix(insuree): correct insuree uuid

--- src/actions.js
+++ src/actions.js
@@ -288,7 +288,7 @@ function formatInsureePhoto(photo) {
 
 export function formatInsureeGQL(mm, insuree) {
   return `
-    ${insuree.uuid !== undefined && insuree.uuid !== null ? `uuid: "234234342342"` : ""}
+    ${insuree.uuid !== undefined && insuree.uuid !== null ? `uuid: "${insuree.uuid}"` : ""}
     ${!!insuree.chfId ? `chfId: "${formatGQLString(insuree.chfId)}"` : ""}
     ${!!insuree.lastName ? `lastName: "${formatGQLString(insuree.lastName)}"` : ""}
     ${!!insuree.otherNames ? `otherNames: "${formatGQLString(insuree.otherNames)}"` : ""}
